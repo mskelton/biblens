30ba094ffc263c889afff4102dbc34c0690b5e8b
Mark Skelton <mdskelton99@gmail.com>
2023-07-19 22:54:50 -0500

    Start work on tests

---
+++ test/verse_list_item_test.dart
@@ -0,0 +1,67 @@
+import 'package:biblens/views/verse_list_item_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:reference_parser/reference_parser.dart';
+
+void main() {
+  testWidgets('Displays a single verse', (WidgetTester tester) async {
+    var ref = parseReference('John 3:16');
+    await tester.pumpWidget(VerseListItemTest(ref: ref));
+
+    expect(find.textContaining('For God so loved'), findsOneWidget);
+    expect(find.text('Open in Bible App'), findsNothing);
+
+    await tester.tap(find.text('John 3:16'));
+    await tester.pump();
+
+    expect(find.textContaining('everlasting life'), findsOneWidget);
+    expect(find.text('Open in Bible App'), findsOneWidget);
+  });
+
+  testWidgets('Displays multiple verses', (WidgetTester tester) async {
+    var ref = parseReference('John 3:16-17');
+    await tester.pumpWidget(VerseListItemTest(ref: ref));
+
+    expect(find.textContaining('For God so loved'), findsOneWidget);
+    expect(find.textContaining('condemn the world'), findsNothing);
+    expect(find.text('Open in Bible App'), findsNothing);
+
+    await tester.tap(find.text('John 3:16-17'));
+    await tester.pump();
+
+    expect(find.textContaining('condemn the world'), findsOneWidget);
+    expect(find.textContaining('everlasting life'), findsOneWidget);
+    expect(find.text('Open in Bible App'), findsOneWidget);
+  });
+
+  testWidgets('Handles invalid references', (WidgetTester tester) async {
+    var ref = Reference("Oops", 1, 1, 1, 1);
+    await tester.pumpWidget(VerseListItemTest(ref: ref));
+
+    expect(find.textContaining('Passage not found'), findsOneWidget);
+    expect(find.text('Open in Bible App'), findsNothing);
+
+    await tester.tap(find.text('John 3:16'));
+    await tester.pump();
+
+    expect(find.text('Open in Bible App'), findsNothing);
+  });
+}
+
+class VerseListItemTest extends StatelessWidget {
+  const VerseListItemTest({Key? key, required this.ref}) : super(key: key);
+
+  final Reference ref;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView.builder(
+          itemCount: 1,
+          itemBuilder: (context, index) => VerseListItem(ref: ref),
+        ),
+      ),
+    );
+  }
+}
